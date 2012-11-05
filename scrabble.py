93c6e2ad4b0fa57e6337989e5d39d3163c5f2b18
Arthur Gavkaliuk <agavkalyuk@malkosua.com>
2012-11-05 19:27:46 +0200

    add some heroku reqs

--- scrabble.py
+++ scrabble.py
@@ -53,4 +53,5 @@ def chars():
     return app.response_class(json.dumps(alphabet), mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
